a9070c192ae6edaae39dedb0168da5f5280c84e7
Brenda <bee@bee.com>
2022-04-24 23:35:40 +0300

    Adds a delete and display user method:

--- user.py
+++ user.py
@@ -12,6 +12,14 @@ class User:
     def save_user(self):
         User.user_list.append(self)
 
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+    def delete_user(self):
+        ''' Deletes an account from the list'''
+        User.user_list.remove(self)
+
     
 
     
