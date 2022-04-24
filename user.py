9f63d1bfa51100991fd7ddf5bd31871c583074ef
Brenda <bee@bee.com>
2022-04-24 10:56:43 +0300

    CReates the user class

---
+++ user.py
@@ -0,0 +1,12 @@
+class Users:
+
+    user_list = []
+
+    '''
+    Class that creates nre users of password locker
+    '''
+    def __init__(self,user_name, password):
+        self.user_name = user_name
+        self.password = password
+
+    
