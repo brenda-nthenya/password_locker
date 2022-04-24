8d67106311911c6d9b24dca6a9e6b56623680033
Brenda <bee@bee.com>
2022-04-24 13:13:56 +0300

    Test for initialization of user

--- user_test.py
+++ user_test.py
@@ -1,5 +1,6 @@
 import unittest
 from user import User
+from credentials import Credentials
 
 class TestUser(unittest.TestCase):
 
@@ -16,10 +17,8 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.user_name, "Brenda")
         self.assertEqual(self.new_user.password, "Qwerty")
 
-    def test_save_user(self):
-        self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1)
 
+         
     
 
 if __name__ == '__main__':
