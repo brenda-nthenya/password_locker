1e0fb994a8c32131f72f8dec159273249b4d0aca
Brenda <bee@bee.com>
2022-04-24 20:51:47 +0300

    Adds the find_credential method

--- credentials.py
+++ credentials.py
@@ -15,3 +15,10 @@ class Credentials:
     def delete_credential(self):
         '''Deletes a credential object'''
         Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def find_credential(cls,account):
+        '''Method takes an account name and returns credentials that match'''
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return credential
