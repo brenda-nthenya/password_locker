8a17aed18269b5652797f2ce6b39df16cc4cb14a
Brenda <bee@bee.com>
2022-04-24 13:14:42 +0300

    Test for saving user

--- user_test.py
+++ user_test.py
@@ -17,6 +17,11 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.user_name, "Brenda")
         self.assertEqual(self.new_user.password, "Qwerty")
 
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+
 
          
     
