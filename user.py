a323cf1534b334bc576b68cdcd9aa96253c00964
Brenda <bee@bee.com>
2022-04-24 20:39:13 +0300

    COrrects errors

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-class Users:
+class User:
 
     user_list = []
 
@@ -8,5 +8,10 @@ class Users:
     def __init__(self,user_name, password):
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+        User.user_list.append(self)
+
+    
 
     
